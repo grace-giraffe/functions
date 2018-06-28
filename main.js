7be03e7f24afcb3eead69ea42bfebfca0a2cb3f7
grace <gracewebb20619@gmail.com>
2018-06-28 13:36:27 -0400

    WHY?

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ var sum = new Function('a', 'b', 'return a+b');
 console.log(sum(2, 4));
 
 //Multiply
-
+var Multiply = 48;   
+for(var i=8; i=48; i=(sum(2,4))*i);
+console.log(i);
 //Power
 
 //Factorial
