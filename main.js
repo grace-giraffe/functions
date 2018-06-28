7d660b0d08a85958aadfc5ffa039dfc8cadeb456
grace <gracewebb20619@gmail.com>
2018-06-28 14:43:49 -0400

    i can do this

--- main.js
+++ main.js
@@ -3,9 +3,8 @@ var sum = new Function('a', 'b', 'return a+b');
 console.log(sum(2, 4));
 
 //Multiply
-var Multiply = 48;   
-for(var i=8; i=48; i=(sum(2,4))*i);
-console.log(i);
+for(i=(sum(2, 4));i<=48; i=i*8);
+console.log(i)
 //Power
 
 //Factorial
