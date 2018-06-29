c63f8d10304e63ba4167dd7154fff63e0331a39b
grace <gracewebb20619@gmail.com>
2018-06-28 21:06:32 -0400

    omg

--- main.js
+++ main.js
@@ -1,12 +1,33 @@
 //Add
-var sum = new Function('a', 'b', 'return a+b');
-console.log(sum(2, 4));
+function add(x,y){
+    return x+y;
+
+}
+document.write(add(2,4)+"<br>")
 
 //Multiply
-for(i=(sum(2, 4));i<=48; i=i*8);
-console.log(i);
+function multiply(x,y){
+    let total=0;
+    for(var i=1;i<y;i++){
+        total=add(x,total)
+    }
+    return total
+}
+document.write(multiply(6,8)+"<br>")
 //Power
-
+function power(x,y){
+    let total=1;
+    for(var i=1;i<y;i++){
+        total=multiply(x,total)
+    }
+    return total
+}
+document.write(power(2,8)+"<br>")
 //Factorial
+function factorial(x){
 
-//Fibonacci 
+}
+//Fibonacci
+function fibonacci(x){
+
+}
